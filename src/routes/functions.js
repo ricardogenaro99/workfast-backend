87f54f51763980419185d81cd172dc50e0b79057
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-06-11 19:01:13 -0500

    feat: changed endpoint

--- src/routes/functions.js
+++ src/routes/functions.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 exports.generateRouterModule = (controller, path) => {
 	const router = express.Router();
-
-	router.get(`/${path}`, controller.getAllData);
-	router.get(`/${path}/:id`, controller.getAllData);
-	router.post(`/${path}`, controller.insertData);
-	router.put(`/${path}/:id`, controller.updateData);
-	router.delete(`/${path}/:id`, controller.deleteData);
+	const apiPath = `/api/${path}`;
+	
+	router.get(`${apiPath}`, controller.getAllData);
+	router.get(`${apiPath}/:id`, controller.getAllData);
+	router.post(`${apiPath}`, controller.insertData);
+	router.put(`${apiPath}/:id`, controller.updateData);
+	router.delete(`${apiPath}/:id`, controller.deleteData);
 
 	return router;
 };
